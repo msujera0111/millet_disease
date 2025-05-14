db3f028b5b221361f1e9f870944145ba8013b572
MRIDUL <mridulsujera@gmail.com>
2025-05-14 07:57:02 +0530

    text and csv changes

--- app.py
+++ app.py
@@ -113,5 +113,5 @@ def market():
     )
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  
-    app.run(host='0.0.0.0', port=port, debug=False)
+    port = int(5000)  
+    app.run(host='0.0.0.0', port=port)
